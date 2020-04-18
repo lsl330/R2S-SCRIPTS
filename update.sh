d6f4d11e3e5b29c13fd9d78109b18ef93cf317ec
lsl330 <54297603+lsl330@users.noreply.github.com>
2020-04-18 22:13:58 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -84,9 +84,11 @@ while [ $checknet -eq 0 ]
 	do
 		echo
 		echo "...........欢迎使用 R2S 一键升级脚本.........."
-		echo " 1. 写入防掉线脚本（开机自动运行）"
+		echo " 1. 写入防掉线脚本并与凌晨4点重启网络（开机自动运行）"
 		echo
-		echo " 2. 不写入"
+		echo " 2. 写入防掉线脚本（开机自动运行）"
+		echo
+		echo " 3. 不写入"
 		echo
 		echo
 		read -p "$(echo -e "请选择 [\e[95m1-2\e[0m]，默认为1:")" checknet
@@ -96,6 +98,8 @@ while [ $checknet -eq 0 ]
 			checknet=1;;
 		2)
 			checknet=2;;
+		3)
+			checknet=3;;
 		*)
 			checknet=0
 			echo
@@ -315,10 +319,16 @@ elif [ -f /bin/update.sh ]; then
 	cp	/bin/update.sh /mnt/img/bin/
 	echo -e '\e[92m写入升级脚本\e[0m'
 fi
-if [ $checknet -eq 1 ]; then   #写入防掉线脚本（开机启动）
+if [ $checknet -le 2 ]; then   #写入防掉线脚本
 	wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/checkwan.sh -O /mnt/img/bin/checkwan.sh
 	chmod +x /mnt/img/bin/checkwan.sh
-	wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
+	if [ $checknet -eq 1 ]; then   #写入防掉线脚本并凌晨四点重启网络（开机启动）
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/restartnetwork.sh -O /mnt/img/bin/restartnetwork.sh
+		chmod +x /mnt/img/bin/restartnetwork.sh
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check2  -O /etc/init.d/check
+	else
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
+	fi
 	chmod 777 /etc/init.d/check
 	ln -s /etc/init.d/check /etc/rc.d/S95check
 	cp	/etc/init.d/check /mnt/img/etc/init.d/check
