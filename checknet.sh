bf73c4d5ba197a05b052ebe22b51af0003ad9edd
lsl330 <54297603+lsl330@users.noreply.github.com>
2020-04-22 15:54:42 +0800

    Update checknet.sh

--- checknet.sh
+++ checknet.sh
@@ -4,16 +4,17 @@ checknet=0;
 while [ $checknet -eq 0 ]
 	do
 		echo
-		echo "...........欢迎使用 R2S 一键升级脚本.........."
+		echo "...........欢迎使用 R2S 防断线脚本.........."
+		echo " 防断线脚本是为了处理pppoe模式下网络断连和防火墙崩溃的折中解决方案，若没上述问题，请谨慎刷入"
+		echo		
 		echo " 1. 写入防掉线脚本并于凌晨4点重启网络（开机自动运行）"
 		echo
 		echo " 2. 写入防掉线脚本（开机自动运行）"
 		echo
 		echo " 3. 不写入"
 		echo
-		echo
-		read -p "$(echo -e "请选择 [\e[95m1-2\e[0m]，默认为1:")" checknet
-		[[ -z $checknet ]] && mode="1"
+		read -p "$(echo -e "请选择 [\e[95m1-2\e[0m]，默认为3:")" checknet
+		[[ -z $checknet ]] && mode="3"
 		case $checknet in
 		1)
 			checknet=1;;
@@ -36,9 +37,21 @@ if [ $checknet -le 2 ]; then   #写入防掉线脚本
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/restartnetwork.sh -O /bin/restartnetwork.sh
 		chmod +x /bin/restartnetwork.sh
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check2  -O /etc/init.d/check
+		if `ps | grep checkwan.sh |grep /|cut -d % -f2`; then
+			nohup sh /bin/restartnetwork.sh  1>/dev/null 2>&1 &
+			echo '检测到网络重启脚本未运行，现已运行脚本'
+		else
+			echo '网络重启脚本已运行'
+		fi
 	else
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
 	fi
 	chmod 777 /etc/init.d/check
 	ln -s /etc/init.d/check /etc/rc.d/S95check
+	if `ps | grep checkwan.sh |grep /|cut -d % -f2`; then
+		nohup sh /bin/checkwan.sh  1>/dev/null 2>&1 &
+		echo '检测到防断线脚本未运行，现已运行脚本'
+	else
+		echo '防断线脚本已运行'
+	fi
 fi
