3b8750a8a83f7df96cedf1fa9a8079688146a8dc
lsl330 <54297603+lsl330@users.noreply.github.com>
2020-05-10 20:17:00 +0800

    Update checkwan.sh

--- checkwan.sh
+++ checkwan.sh
@@ -4,34 +4,44 @@
 echo '脚本检测开始'
 dns=119.29.29.29
 tries=0
-wan=`ifconfig |grep inet| sed -n '1p'|awk '{print $2}'|awk -F ':' '{print $2}'`
 while true
 do
 # do something
-if ping -w 1 -c 1 $dns; then #ping dns通则
-	echo '网络正常'
-	tries=0
-	dns=119.29.29.29
+wan=`ifconfig |grep inet| sed -n '1p'|awk '{print $2}'|awk -F ':' '{print $2}'`
+if expr match "`ifconfig |grep pppoe-wan`" 'pppoe' >> /dev/null 2>&1; then
+	if ping -w 1 -c 1 $dns; then #ping dns通则
+		echo '网络正常'
+		tries=0
+		dns=119.29.29.29
+	else
+		sleep 1
+		if ping -w 1 -c 1 $wan; then #若ping网关通则
+			echo '网关正常'
+			tries=$((tries+1))
+			if [ $tries -eq 2 ]; then
+				dns=114.114.114.114
+			elif [ $tries -eq 4 ]; then
+				dns=223.5.5.5
+			fi
+		else #ping不通，重启防火墙
+			echo '网络崩溃重启防火墙'
+			/etc/init.d/firewall reload
+			sleep 10
+		fi
+		if [ $tries -ge 5 ]; then #连续ping dns 5次失败，重启wan口
+			tries=0
+			/etc/init.d/network restart
+			sleep 5
+		fi
+	fi
+	sleep 2
 else
-	sleep 1
 	if ping -w 1 -c 1 $wan; then #若ping网关通则
 		echo '网关正常'
-		tries=$((tries+1))
-		if [ $tries -eq 2 ]; then
-			dns=114.114.114.114
-		elif [ $tries -eq 4 ]; then
-			dns=223.5.5.5
-		fi
 	else #ping不通，重启防火墙
 		echo '网络崩溃重启防火墙'
 		/etc/init.d/firewall reload
 		sleep 10
 	fi
-	if [ $tries -ge 5 ]; then #连续ping dns 5次失败，重启wan口
-		tries=0
-		/etc/init.d/network restart
-		sleep 5
-	fi
 fi
-sleep 2
 done
