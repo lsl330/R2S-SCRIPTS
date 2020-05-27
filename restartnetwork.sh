9cefb5e29d98ab96579717595dd4e723ae048c31
lsl330 <54297603+lsl330@users.noreply.github.com>
2020-05-27 09:31:09 +0800

    Update restartnetwork.sh

--- restartnetwork.sh
+++ restartnetwork.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-TIME=$(((`date +%H`*3600)+(`date +%M`*60)+`date +%S`))
+HOUR=$(expr `date +%H` \* 3600)
+MINUTE=$(expr `date +%M` \* 60)
+SECOND=`date +%S`
+TIME=$(expr $HOUR + $MINUTE + $SECOND)
 if [ $TIME -ge 14400 ]; then # 凌晨4点的秒数14400秒，次日凌晨4点的秒数100800秒
-	SECOND=$((100800-TIME))
+	SECOND=$(expr 100800 - $TIME)
 else
-	SECOND=$((14400-TIME))
+	SECOND=$(expr 14400 - $TIME)
 fi
 sleep $SECOND
 /etc/init.d/network restart
