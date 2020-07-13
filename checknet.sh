5f0877566858e715cf2d7ef115bd4913b6e3fc6c
lsl330 <54297603+lsl330@users.noreply.github.com>
2020-07-13 21:10:05 +0800

    Update checknet.sh

--- checknet.sh
+++ checknet.sh
@@ -42,12 +42,12 @@ if ! type "nohup" > /dev/null; then
 fi
 
 if [ $checknet -le 3 ]; then   #写入防掉线脚本
-	wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/checkwan.sh -O /bin/checkwan.sh
-	chmod +x /bin/checkwan.sh
 	if [ $checknet -eq 1 ]; then   #写入防掉线脚本并凌晨四点重启网络（开机启动）
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/restartnetwork.sh -O /bin/restartnetwork.sh
 		chmod +x /bin/restartnetwork.sh
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check2  -O /etc/init.d/check
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/checkwan.sh -O /bin/checkwan.sh
+		chmod +x /bin/checkwan.sh
 		if `ps | grep restartnetwork.sh |grep /|cut -d % -f2`; then
 			sleep 2
 			nohup sh /bin/restartnetwork.sh  1>/dev/null 2>&1 &
@@ -68,10 +68,12 @@ if [ $checknet -le 3 ]; then   #写入防掉线脚本
 		fi
 	else
 		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/checkwan.sh -O /bin/checkwan.sh
+		chmod +x /bin/checkwan.sh
 	fi
 	chmod 777 /etc/init.d/check
 	ln -s /etc/init.d/check /etc/rc.d/S95check
-	if [ $checknet -le 3 ]; then   #写入防掉线脚本
+	if [ $checknet -le 2 ]; then   #写入防掉线脚本
 		if `ps | grep checkwan.sh |grep /|cut -d % -f2`; then
 			sleep 1
 			nohup sh /bin/checkwan.sh  1>/dev/null 2>&1 &
